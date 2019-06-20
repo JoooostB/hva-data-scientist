de178fd1c9909efb35d58eef102a00ebc2d9e92e
Joost Buskermolen <joost@buskervezel.nl>
2019-06-20 15:09:45 +0200

    modify input data to fit analysis

--- constructor/table.py
+++ constructor/table.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from os import path, remove
 
 
 class Table:
@@ -9,6 +10,12 @@ class Table:
         self.df_cols_to_index = df_cols_to_index
 
     def store(self):
+        # Delete hdf file if it exists already.
+        if path.exists(self.hdf_file):
+            try:
+                remove(self.hdf_file)
+            except OSError:
+                pass
         store = pd.HDFStore(self.hdf_file)
         for chunk in pd.read_csv(self.csv_file, chunksize=500000):
             # don't index data columns in each iteration
@@ -16,3 +23,12 @@ class Table:
         # index data columns in HDFStore
         store.create_table_index(self.hdf_key, columns=self.df_cols_to_index, optlevel=9, kind='full')
         store.close()
+        return store
+
+
+    def query(self, query):
+        read = pd.HDFStore(self.hdf_file, mode='r').get(self.hdf_key)
+        return read.query(query)
+
+    def add_sentiment(self, df, value):
+        df['Sentiment'] = value
--- run.py
+++ run.py
@@ -1,8 +1,28 @@
 from constructor.db import Collection
 from constructor.table import Table
 
-coll = Collection('/Users/joost/PycharmProjects/hva-data-scientist/hotel-reviews.csv', 'hotel-reviews')
-coll.fill()
-
 review_table = Table('hotel-reviews.csv', './data/table.h5', 'hotel_reviews')
 review_table.store()
+positive = review_table.query('Reviewer_Score > 5.4')
+#positive.rename(columns={'Positive_Review': 'Review_Text'})
+print(positive)
+positive.drop(['Negative_Review'])
+positive['Sentiment'] = 1
+
+
+negative = review_table.query('Reviewer_Score < 5.5')
+negative.rename(columns={'Negative_Review': 'Review_Text'})
+negative.drop(['Positive_Review'])
+negative['Sentiment'] = 0
+
+positive.to_csv(r'./data/Review_pos.csv', index=None, header=True)
+negative.to_csv(r'./data/Review_neg.csv', index=None, header=True)
+
+df = pd.concat([positive, negative])    # Combine dataframes after adding sentiment
+Collection(df, 'hotel-reviews').fill()    # Fill mongo with new df
+Collection(positive[['Review_Text', 'Sentiment']].sample(n=10000), 'pos').fill()
+Collection(negative[['Review_Text', 'Sentiment']].sample(n=10000), 'neg').fill()
+
+print(df.head())
+
+
