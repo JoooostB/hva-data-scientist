5a205306c67ede0c7c92c45f8818d0b57caa46e8
Joost Buskermolen <joost@buskervezel.nl>
2019-07-24 13:59:43 +0200

    solved fill order

--- fill.py
+++ fill.py
@@ -6,14 +6,14 @@ review_table = Table('hotel-reviews.csv', './data/table.h5', 'hotel_reviews')
 review_table.store()
 
 positive = review_table.query('Reviewer_Score > 5.4')
-positive['Sentiment'] = 1
 negative = review_table.query('Reviewer_Score < 5.5')
+positive['Sentiment'] = 1
 negative['Sentiment'] = 0
 
 positive.rename(columns={'Positive_Review': 'Review_Text'}, inplace=True)
-positive.drop("Negative_Review", axis=1)
 negative.rename(columns={'Negative_Review': 'Review_Text'}, inplace=True)
-negative.drop("Positive_Review", axis=1)
+positive.drop(["Negative_Review"], axis=1)
+negative.drop(["Positive_Review"], axis=1)
 
 positive.to_csv(r'./data/Review_pos.csv', index=None, header=True)
 negative.to_csv(r'./data/Review_neg.csv', index=None, header=True)
@@ -25,5 +25,3 @@ Collection(positive[['Review_Text', 'Sentiment']].sample(n=10000), 'pos').fill()
 Collection(negative[['Review_Text', 'Sentiment']].sample(n=10000), 'neg').fill()
 
 print(df.head())
-
-
