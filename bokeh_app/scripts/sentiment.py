8aaee2e1985294d35f857780b631d9d379c8b884
Joost Buskermolen <joost@buskervezel.nl>
2019-08-26 19:26:21 +0100

    code cleanup

--- bokeh_app/scripts/sentiment.py
+++ bokeh_app/scripts/sentiment.py
@@ -17,17 +17,17 @@ def sentiment_tab(df):
     }
     chart_colors = ['#DAF7A6', '#FF5733']
 
-    data = pd.Series(x).reset_index(name='value').rename(columns={'index':'country'})
+    data = pd.Series(x).reset_index(name='value').rename(columns={'index':'sentiment'})
     data['angle'] = data['value']/data['value'].sum() * 2*pi
 
     data['color'] = chart_colors[:len(x)]
 
     p = figure(plot_height=350, title="Sentiment Chart", toolbar_location=None,
-               tools="hover", tooltips="@country: @value", x_range=(-0.5, 1.0))
+               tools="hover", tooltips="@sentiment: @value", x_range=(-0.5, 1.0))
 
     p.wedge(x=0, y=1, radius=0.4,
             start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
-            line_color="white", fill_color='color', legend='country', source=data)
+            line_color="white", fill_color='color', legend='sentiment', source=data)
 
     p.axis.axis_label=None
     p.axis.visible=False
